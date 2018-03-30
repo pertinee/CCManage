8bb1a0e379c3608c6b646d56a2bee3722ee64bbd
1090061055@qq.com <1090061055@qq.com>
2018-03-30 22:28:54 +0800

    更新代码规范

--- src/main/java/com/lcz/manage/CCManageApplication.java
+++ src/main/java/com/lcz/manage/CCManageApplication.java
@@ -1,14 +1,18 @@
 package com.lcz.manage;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.lcz.manage.*.dao")
 public class CCManageApplication {
+	private static Logger log = LoggerFactory.getLogger(CCManageApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CCManageApplication.class, args);
+		log.info("========== CCManage启动成功 ==========");
 	}
 }
