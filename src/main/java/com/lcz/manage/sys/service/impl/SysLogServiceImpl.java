0a5ab505b74947de436fe6b25979109816adb911
1090061055@qq.com <1090061055@qq.com>
2018-03-31 13:15:59 +0800

    更新README

--- src/main/java/com/lcz/manage/sys/service/impl/SysLogServiceImpl.java
+++ src/main/java/com/lcz/manage/sys/service/impl/SysLogServiceImpl.java
@@ -14,50 +14,50 @@ import java.util.Map;
 
 @Service("sysLogService")
 public class SysLogServiceImpl implements SysLogService {
-	@Autowired
-	private SysLogDao sysLogDao;
-	
-	@Override
-	public SysLogBean queryObject(String id){
-		return sysLogDao.queryObject(id);
-	}
-	
-	@Override
-	public List<SysLogBean> queryList(Map<String, Object> map){
-		return sysLogDao.queryList(map);
-	}
+    @Autowired
+    private SysLogDao sysLogDao;
 
-	@Override
-	public List<SysLogBean> queryLogDaily(){
-		return sysLogDao.queryLogDaily();
-	}
-	
-	@Override
-	public int queryTotal(Map<String, Object> map){
-		return sysLogDao.queryTotal(map);
-	}
-	
-	@Override
-	public void save(SysLogBean sysLogBean){
-		if(StringUtils.isEmpty(sysLogBean.getId())){
-			sysLogBean.setId(IdUtils.uuid());
-		}
-		sysLogDao.save(sysLogBean);
-	}
-	
-	@Override
-	public void update(SysLogBean sysLogBean){
-		sysLogDao.update(sysLogBean);
-	}
-	
-	@Override
-	public void delete(String id){
-		sysLogDao.delete(id);
-	}
-	
-	@Override
-	public void deleteBatch(String[] ids){
-		sysLogDao.deleteBatch(ids);
-	}
-	
+    @Override
+    public SysLogBean queryObject(String id) {
+        return sysLogDao.queryObject(id);
+    }
+
+    @Override
+    public List<SysLogBean> queryList(Map<String, Object> map) {
+        return sysLogDao.queryList(map);
+    }
+
+    @Override
+    public List<SysLogBean> queryLogDaily() {
+        return sysLogDao.queryLogDaily();
+    }
+
+    @Override
+    public int queryTotal(Map<String, Object> map) {
+        return sysLogDao.queryTotal(map);
+    }
+
+    @Override
+    public void save(SysLogBean sysLogBean) {
+        if (StringUtils.isEmpty(sysLogBean.getId())) {
+            sysLogBean.setId(IdUtils.uuid());
+        }
+        sysLogDao.save(sysLogBean);
+    }
+
+    @Override
+    public void update(SysLogBean sysLogBean) {
+        sysLogDao.update(sysLogBean);
+    }
+
+    @Override
+    public void delete(String id) {
+        sysLogDao.delete(id);
+    }
+
+    @Override
+    public void deleteBatch(String[] ids) {
+        sysLogDao.deleteBatch(ids);
+    }
+
 }
