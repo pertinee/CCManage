924c79b7c78043f6d10c027c96653799b2b8cb19
1090061055@qq.com <1090061055@qq.com>
2018-10-21 15:04:26 +0800

    添加List集合工具类

---
+++ src/main/java/com/lcz/manage/util/ListUtils.java
@@ -0,0 +1,94 @@
+/**
+ * Hangzhou Hundsun Cloud Financing Network Technology Co.,Ltd.
+ * Copyright 2015-2018 | www.hsjry.com . All rights reserved .
+ */
+package com.lcz.manage.util;
+
+import org.apache.log4j.Logger;
+
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 说明:List集合工具类
+ *
+ * @author luchunzhou
+ * @date 2018年10月21日 下午2:31:30
+ */
+public class ListUtils {
+
+    private static Logger logger = Logger.getLogger(ListUtils.class);
+
+    /**
+     * List转换为map
+     *
+     * @param list  数据源
+     * @param idFieldName   主键标识字段，作为返回map的key
+     * @param <K>
+     * @param <T>
+     * @return
+     */
+    public static <K, T> Map<K, List<T>> list2Map(List<T> list, String idFieldName) {
+        Map<K, List<T>> result = new HashMap<>();
+        for (T item : list) {
+            Object key = obtainIdValue(item, idFieldName);
+            if (key == null) {
+                continue;
+            }
+            List<T> optList = result.get(key);
+            if (optList == null) {
+                optList = new ArrayList<T>();
+            }
+            optList.add(item);
+            result.put((K) key, optList);
+        }
+        return result;
+    }
+
+//    public static <T> Map<Long, List<T>> list2Map(List<T> list, String idFieldName) {
+//        Map<Long, List<T>> result = new HashMap<>();
+//        for (T item : list) {
+//            Object key = obtainIdValue(item, idFieldName);
+//            if (key == null) {
+//                continue;
+//            }
+//            List<T> optList = result.get(key);
+//            if (optList == null) {
+//                optList = new ArrayList<T>();
+//            }
+//            optList.add(item);
+//            result.put((Long) key, optList);
+//        }
+//        return result;
+//    }
+
+
+    /**
+     * 根据字段名获取对象中主键
+     *
+     * @throws
+     * @parameter：@param item 对象
+     * @parameter：@param idFieldName 字段名
+     * @parameter：@return
+     * @return：Long
+     */
+    public static <T> Object obtainIdValue(T item, String idFieldName) {
+        try {
+            Class<? extends Object> clazz = item.getClass();
+            Field field = clazz.getDeclaredField(idFieldName);
+            PropertyDescriptor descriptor = new PropertyDescriptor(field.getName(), clazz);
+            Method method = descriptor.getReadMethod();
+            Object valObj = method.invoke(item);
+            return valObj == null ? null : valObj;
+        } catch (Exception e) {
+            logger.error("获取对象主键失败", e);
+            return null;
+        }
+    }
+}
+
